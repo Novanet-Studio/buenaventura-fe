a36e0d07a793ff926d6cc93e09911b89c754ad10
Davejs136 <davejs136@gmail.com>
2020-10-23 16:48:22 -0400

    add: Layout page

---
+++ src/components/footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => (
+  <footer className="footer">
+    <div className="footer__rsss">
+      <div className="footer__item">
+        <div className="footer__link">LinkedIn</div>
+      </div>
+      <div className="footer__item">
+        <div className="footer__link">Instagram</div>
+      </div>
+      <div className="footer__item">
+        <div className="footer__link">Twitter</div>
+      </div>
+    </div>
+    <p className="footer__copy">
+      © Derechos reservados {new Date().getFullYear()} | Diseñado por: <img className="footer__logo-novanet" src="" alt="logo-novanet" />
+    </p>
+  </footer>
+)
+
+export default Footer
