6f6300a087ede2828722587b73e247ce4c376ba1
Davejs136 <davejs136@gmail.com>
2020-10-24 13:31:13 -0400

    feat: intl support

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -1,30 +1,49 @@
-import React from 'react'
+import React from "react"
+import { useIntl } from "../../plugins/gatsby-plugin-intl-graphql"
 
-const Contacto = () => (
-  <section className="contacto">
-    <h2 className="contact__titulo">Lorem, ipsum</h2>
-    <p className="contacto__descripcion">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex fugit ipsam
-      amet ipsa assumenda hic quasi quibusdam dignissimos aspernatur libero.
-    </p>
-    <div className="contacto__fila">
-      <img src="" alt="lapicero rojo" className="imagen" />
-      <form className="contact__formulario">
-        <div className="contacto__formulario-grupo">
-          <input type="text" placeholder="Nombre" className="contacto__input" />
-          <input type="text" placeholder="Apellido" className="contacto__input" />
-        </div>
-        <div className="contacto__formulario-grupo">
-          <input type="text" placeholder="Teléfono" className="contacto__input" />
-          <input type="email" placeholder="Correo electrónico" className="contacto__input" />
-        </div>
-        <input type="textarea" className="contacto__textarea" />
-        <button className="boton">
-          <p className="boton__texto">Enviar información</p>
-        </button>
-      </form>
-    </div>
-  </section>
-)
+const Contacto = () => {
+
+  const { messages: intl } = useIntl()
+
+  return (
+    <section className="contacto">
+      <h2 className="contact__titulo">{intl.contacto.content.principal.titulo}</h2>
+      <p className="contacto__descripcion">{intl.contacto.content.principal.descripcion}</p>
+      <div className="contacto__fila">
+        <img src="" alt="lapicero rojo" className="imagen" />
+        <form className="contact__formulario">
+          <div className="contacto__formulario-grupo">
+            <input
+              type="text"
+              placeholder="Nombre"
+              className="contacto__input"
+            />
+            <input
+              type="text"
+              placeholder="Apellido"
+              className="contacto__input"
+            />
+          </div>
+          <div className="contacto__formulario-grupo">
+            <input
+              type="text"
+              placeholder="Teléfono"
+              className="contacto__input"
+            />
+            <input
+              type="email"
+              placeholder="Correo electrónico"
+              className="contacto__input"
+            />
+          </div>
+          <input type="textarea" className="contacto__textarea" />
+          <button className="boton">
+            <p className="boton__texto">Enviar información</p>
+          </button>
+        </form>
+      </div>
+    </section>
+  )
+}
 
 export default Contacto
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,31 @@
 import React from "react"
-
+import { useIntl } from "../../plugins/gatsby-plugin-intl-graphql"
 import Layout from "../components/layout"
 
-const IndexPage = () => (
-  <Layout>
-    <section className="inicio">
-      <article className="contenedor contenedor--izq">
-        <h1 className="inicio__titulo">Tenga sus impuesto</h1>
-        <p className="inicio__descripcion">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate
-          debitis commodi accusamus vero libero quasi quas facere consequuntur
-          molestiae. Eligendi cupiditate eveniet est, dicta animi sequi
-          architecto maiores quod exercitationem?
-        </p>
-        <button className="button">
-          <p className="button__texto">Conócenos</p>
-        </button>
-      </article>
-      <article className="contenedor contenedor--der">
-        <img src="" alt="dos personas sentadas" className="imagen" />
-      </article>
-    </section>
-  </Layout>
-)
+const IndexPage = () => {
+
+  const { messages: intl } = useIntl()
+
+  console.log(intl)
+
+  return (
+    <Layout>
+      <section className="inicio">
+        <article className="contenedor contenedor--izq">
+          <h1 className="inicio__titulo">{intl.principal.content.principal.titulo}</h1>
+          <p className="inicio__descripcion">
+            {intl.principal.content.principal.descripcion}
+          </p>
+          <button className="button">
+            <p className="button__texto">{intl.principal.content.cta_titulo}</p>
+          </button>
+        </article>
+        <article className="contenedor contenedor--der">
+          <img src="" alt="dos personas sentadas" className="imagen" />
+        </article>
+      </section>
+    </Layout>
+  )
+}
 
 export default IndexPage
