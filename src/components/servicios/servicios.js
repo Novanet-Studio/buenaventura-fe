8075b8d54051036fae84fc0cc7081b7d451dfc3e
RobertH-O <robertohernandez2610@gmail.com>
2021-02-05 13:30:53 -0400

    Aplicación de estilos nuevos a "Servicios adicionales"

--- src/components/servicios/servicios.js
+++ src/components/servicios/servicios.js
@@ -27,36 +27,40 @@ const Servicios = () => {
         image={intl.messages.serviciosAdicionale.content.seo.imagen}
       />
       <section id="Servicios" className="servicios">
-        <h2 className="titulo">
-          {intl.messages.serviciosAdicionale.content.principal.titulo}
-        </h2>
-        <p className="descripcion">
-          {intl.messages.serviciosAdicionale.content.principal.descripcion}
-        </p>
-        <ul className="servicios__lista">
-          {intl.messages.serviciosAdicionale.content.servicios.map(
-            (servicio, index) => (
-              <li className="servicios__contenido" key={servicio.id}>
-                <img
-                  className="servicios__icono"
-                  src={strapiServiciosAdicionales.iconos[index].url}
-                  title={
-                    intl.messages.serviciosAdicionale.content.seo_imagen[index]
-                      .titulo
-                  }
-                  alt={
-                    intl.messages.serviciosAdicionale.content.seo_imagen[index]
-                      .alt
-                  }
-                />
-                <h3 className="servicios__contenido-titulo">
-                  {servicio.nombre}
-                </h3>
-                <ReactMarkdown source={servicio.items} escapeHtml={false} />
-              </li>
-            )
-          )}
-        </ul>
+        <div className="contenedor-principal">
+          <h2 className="titulo">
+            {intl.messages.serviciosAdicionale.content.principal.titulo}
+          </h2>
+          <p className="descripcion">
+            {intl.messages.serviciosAdicionale.content.principal.descripcion}
+          </p>
+          <ul className="servicios__lista">
+            {intl.messages.serviciosAdicionale.content.servicios.map(
+              (servicio, index) => (
+                <li className="servicios__contenido" key={servicio.id}>
+                  <img
+                    className="servicios__icono"
+                    src={strapiServiciosAdicionales.iconos[index].url}
+                    title={
+                      intl.messages.serviciosAdicionale.content.seo_imagen[
+                        index
+                      ].titulo
+                    }
+                    alt={
+                      intl.messages.serviciosAdicionale.content.seo_imagen[
+                        index
+                      ].alt
+                    }
+                  />
+                  <h3 className="servicios__contenido-titulo">
+                    {servicio.nombre}
+                  </h3>
+                  <ReactMarkdown source={servicio.items} escapeHtml={false} />
+                </li>
+              )
+            )}
+          </ul>
+        </div>
       </section>
     </>
   )
