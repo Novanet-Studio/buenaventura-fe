33ef94625f1bbeef7cda5bade3c08df001a7dee2
RobertH-O <robertohernandez2610@gmail.com>
2021-02-05 16:48:21 -0400

    Aplicación de estilos nuevos en "Contacto"

--- src/components/contacto/contacto.js
+++ src/components/contacto/contacto.js
@@ -32,67 +32,67 @@ const Contacto = () => {
         image={intl.messages.contacto.content.seo.imagen}
       />
       <section id="Contacto" className="contacto">
-        <h2 className="titulo">
-          {intl.messages.contacto.content.principal.titulo}
-        </h2>
-        <p className="descripcion">
-          {intl.messages.contacto.content.principal.descripcion}
-        </p>
-        <div className="contenedor--izq">
-          <Img
-            className="contacto__imagen"
-            fluid={strapiContacto.imagen.localFile.childImageSharp.fluid}
-            title={intl.messages.contacto.content.seo_imagen.titulo}
-            alt={intl.messages.contacto.content.seo_imagen.alt}
-          />
-        </div>
-        <div className="contenedor--der">
-          <form className="contact__formulario">
-            <div className="contacto__formulario-grupo">
-              <input
-                type="text"
-                placeholder={
-                  intl.messages.static.contacto.formulario.nombrePlaceholder
-                }
-                className="contacto__input-text"
-              />
-              <input
-                type="text"
-                placeholder={
-                  intl.messages.static.contacto.formulario.apellidoPlaceholder
-                }
-                className="contacto__input-text"
+        <div className="contenedor-principal">
+          <h2 className="titulo">
+            {intl.messages.contacto.content.principal.titulo}
+          </h2>
+          <p className="descripcion">
+            {intl.messages.contacto.content.principal.descripcion}
+          </p>
+          <div className="contacto__contenido">
+            <div className="contenedor--izq">
+              <form className="contacto__formulario">
+                <input
+                  type="text"
+                  placeholder={
+                    intl.messages.static.contacto.formulario.nombrePlaceholder
+                  }
+                  className="contacto__input-text"
+                />
+                <input
+                  type="text"
+                  placeholder={
+                    intl.messages.static.contacto.formulario.apellidoPlaceholder
+                  }
+                  className="contacto__input-text"
+                />
+                <input
+                  type="text"
+                  placeholder={
+                    intl.messages.static.contacto.formulario.telefonoPlaceholder
+                  }
+                  className="contacto__input-text"
+                />
+                <input
+                  type="email"
+                  placeholder={
+                    intl.messages.static.contacto.formulario.correoPlaceholder
+                  }
+                  className="contacto__input-email"
+                />
+                <input
+                  type="textarea"
+                  placeholder={
+                    intl.messages.static.contacto.formulario.mensajePlaceholder
+                  }
+                  className="contacto__textarea"
+                />
+                <button className="boton">
+                  <p className="boton__texto">
+                    {intl.messages.static.contacto.boton}
+                  </p>
+                </button>
+              </form>
+            </div>
+            <div className="contenedor--der">
+              <Img
+                className="contacto__imagen"
+                fluid={strapiContacto.imagen.localFile.childImageSharp.fluid}
+                title={intl.messages.contacto.content.seo_imagen.titulo}
+                alt={intl.messages.contacto.content.seo_imagen.alt}
               />
             </div>
-            <div className="contacto__formulario-grupo">
-              <input
-                type="text"
-                placeholder={
-                  intl.messages.static.contacto.formulario.telefonoPlaceholder
-                }
-                className="contacto__input-text"
-              />
-              <input
-                type="email"
-                placeholder={
-                  intl.messages.static.contacto.formulario.correoPlaceholder
-                }
-                className="contacto__input-email"
-              />
-            </div>
-            <input
-              type="textarea"
-              placeholder={
-                intl.messages.static.contacto.formulario.mensajePlaceholder
-              }
-              className="contacto__textarea"
-            />
-            <button className="boton">
-              <p className="boton__texto">
-                {intl.messages.static.contacto.boton}
-              </p>
-            </button>
-          </form>
+          </div>
         </div>
       </section>
     </>
